b09fc44c6ec7eb57abebe029f9b801f3449d5bc5
Vitali <vitaliy.a.salomatov@gmail.com>
2023-12-20 22:13:28 +0500

    Folder Lesson1 added

---
+++ lesson1/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿int i1 = 10;
+int i2 = 10;
+
+if (i1 > i2)
+{
+    Console.Write("Первое число ");
+    Console.Write(i1);
+    Console.Write(" больше, чем второе число ");
+    Console.Write(i2);
+}
+else
+{
+    if (i1 < i2)
+    {
+        Console.Write("Второе число ");
+        Console.Write(i2);
+        Console.Write(" больше, чем первое число ");
+        Console.Write(i1);
+    }
+    else
+    {
+        Console.Write("Первое число ");
+        Console.Write(i1);
+        Console.Write(" равно второму числу ");
+        Console.Write(i2);
+    }
+}
